26378d2ef2255fcf27ddfd53204daae774afc6a0
Zofia-Szybalska <zochula111@gmail.com>
2023-10-19 13:03:09 +0200

    Added player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,11 @@
-extends Node2D
+extends CharacterBody2D
 
+@export var speed = 200
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	var motion_vector = Input.get_vector("left", "right", "up", "down") * speed * delta
+	if(motion_vector.x < 0):
+		$AnimatedSprite2D.flip_v = true
+	else:
+		$AnimatedSprite2D.flip_v = false
+	move_and_collide(motion_vector)
