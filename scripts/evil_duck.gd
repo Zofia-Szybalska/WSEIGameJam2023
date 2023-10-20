aa69b4d1cfe4a8762a95c268ca75974680507e64
Zofia-Szybalska <zochula111@gmail.com>
2023-10-20 08:51:20 +0200

    Duck is over everything

--- scripts/evil_duck.gd
+++ scripts/evil_duck.gd
@@ -4,8 +4,10 @@ extends Node2D
 func activate():
 	show()
 	$Area2D/CollisionShape2D.set_deferred("disabled", false)
+	z_index = 0
 
 func _on_area_2d_body_entered(_body):
+	z_index = 100
 	var tween = get_tree().create_tween()
 	$AudioStreamPlayer.play()
 	$Timer.start()
