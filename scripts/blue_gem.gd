9b5a1304f6c032e44018e9e1619c28796d22f13c
Mariusz Gajewski <mariusz.gajewski@micorsoft.wsei.edu.pl>
2023-10-20 08:14:34 +0200

    Blue gem sound

--- scripts/blue_gem.gd
+++ scripts/blue_gem.gd
@@ -5,7 +5,10 @@ func _on_body_entered(_body):
 	var gem = get_tree().get_nodes_in_group("blue_gem")
 	get_tree().get_nodes_in_group("evil_duck")[0].activate()
 	gem[0].show()
+	hide()
+	$Pickup.play()
+	set_collision_layer_value(1, false)
 	var cannons = get_tree().get_nodes_in_group("cannon")
 	for cannon in cannons:
 		cannon.appear()
-	queue_free()
+	#queue_free()
