3d353b8c2600dedad09ef5a405ba1075d060ed79
Mariusz Gajewski <mariusz.gajewski@micorsoft.wsei.edu.pl>
2023-10-20 07:35:09 +0200

    Level one sound

--- scripts/red_gem.gd
+++ scripts/red_gem.gd
@@ -1,6 +1,19 @@
 extends Area2D
 
+@export var bg:AudioStreamPlayer
+@export var player:CharacterBody2D
+
+var timer = 8
+
 
 func _on_body_entered(body):
 	if body is CharacterBody2D:
-		get_tree().change_scene_to_file("res://scenes/Level2.tscn")
+		bg.stop()
+		$Pickup.play()
+		$Timer.start()
+		player.speed = 0
+		player.dash_speed = 0
+		
+
+func _on_timer_timeout():
+	get_tree().change_scene_to_file("res://scenes/Level2.tscn")
