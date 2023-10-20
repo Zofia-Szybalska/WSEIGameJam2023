785ceb8a0ed7167e3d61897a7b41306514261cb0
Michał Marchewka <michal.marchewka@microsoft.wsei.edu.pl>
2023-10-20 04:16:37 +0200

    naprawione repo

--- scripts/abyss.gd
+++ scripts/abyss.gd
@@ -1,27 +1,13 @@
 extends Node2D
 
 signal player_entered
+var player
 
-func _on_body_entered(body):
-	if body is CharacterBody2D:
-		if body.dashing:
-			body.is_over_abyss = true
-			return
-		else:
-			body.take_damage()
-	var vector = Vector2(0,0)
-	if body.position.x > position.x:
-		vector.x = 1 
-	if body.position.x <= position.x:
-		vector.x = -1
-	if body.position.y > position.y:
-		vector.y = 1
-	if body.position.y <= position.y:
-		vector.y = -1
-	body.velocity *= vector #well, nie dziala
-	body.move_and_slide()
+func _ready():
+	player = get_parent()
 
+func _on_body_entered(_body):
+	player.is_over_abyss = false
 
-func _on_body_exited(body):
-	if body is CharacterBody2D:
-		body.is_over_abyss = false
+func _on_body_exited(_body):
+	player.is_over_abyss = true
